5e69efaa515797b6cf35ea52aec8ff65df420300
Hussam812 <hossamhosnihussien@gmail.com>
2023-08-03 06:49:30 +0200

    finshed screen of mobile

--- js/script.js
+++ js/script.js
@@ -41,12 +41,18 @@ addeventOnElem(navLinks, "click", closNav);
 /**header Active */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
 
 window.addEventListener("scroll", () => {
     if (window.scrollY >= 100){
-        header.classList.add("active")
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+
     }else(
-        header.classList.remove("active")
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+
 
     )
 });
